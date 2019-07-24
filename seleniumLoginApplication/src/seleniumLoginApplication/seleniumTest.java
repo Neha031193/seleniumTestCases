fe1721c9ab2e8c82755d8e0859adee5873629d5b
Neha031193 <bnehah@gmail.com>
2019-07-24 16:36:15 +0530

    commiting changes related to testNG (working code)

--- seleniumLoginApplication/src/seleniumLoginApplication/seleniumTest.java
+++ seleniumLoginApplication/src/seleniumLoginApplication/seleniumTest.java
@@ -1,35 +1,56 @@
 package seleniumLoginApplication;
-
 import java.io.IOException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class seleniumTest {
-	 
-	        public static void main(String[] args) throws IOException, InterruptedException {
-	                System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-	 
-	                WebDriver driver = new ChromeDriver();
-	 
-	                driver.get("http://127.0.0.1:5500/");
-	                Thread.sleep(1000);
-	                driver.findElement(By.linkText("Register")).click();
-	 
-	                Thread.sleep(1000);
-	                driver.findElement(By.id("validationCustom01")).sendKeys("abcd");
-	                driver.findElement(By.id("validationCustom02")).sendKeys("efgh");
-	                driver.findElement(By.id("validationCustomUsername")).sendKeys("abc@gmail.com");
-	                driver.findElement(By.id("validationCustom03")).sendKeys("9878906789");
-	                driver.findElement(By.id("validationCustom04")).sendKeys("abcd");
-	    
-	                driver.findElement(By.id("submitRegButton")).click();
-	                
-	                
-	              
-	                //driver.quit();
-	        }
+//	        public static void main(String[] args) throws IOException, InterruptedException {
+//	                System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+//	 
+//	                WebDriver driver = new ChromeDriver();
+//	 
+//	                driver.get("http://127.0.0.1:5500/");
+//	                Thread.sleep(1000);
+//	                driver.findElement(By.linkText("Register")).click();
+//	 
+//	                Thread.sleep(1000);
+//	                driver.findElement(By.id("validationCustom01")).sendKeys("abcd");
+//	                driver.findElement(By.id("validationCustom02")).sendKeys("efgh");
+//	                driver.findElement(By.id("validationCustomUsername")).sendKeys("abc@gmail.com");
+//	                driver.findElement(By.id("validationCustom03")).sendKeys("9878906789");
+//	                driver.findElement(By.id("validationCustom04")).sendKeys("abcd");
+//	    
+//	                driver.findElement(By.id("submitRegButton")).click();
+//	                
+//	                
+//	              
+//	                //driver.quit();
+//	        }
+	@Test
+	public void seleniumTest1() throws IOException, InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		 
+		                WebDriver driver = new ChromeDriver();
+		 
+		                driver.get("http://127.0.0.1:5500/");
+		                Thread.sleep(1000);
+		                driver.findElement(By.linkText("Register")).click();
+		 
+		                Thread.sleep(1000);
+		                driver.findElement(By.id("validationCustom01")).sendKeys("abcd");
+		                driver.findElement(By.id("validationCustom02")).sendKeys("efgh");
+		                driver.findElement(By.id("validationCustomUsername")).sendKeys("abc@gmail.com");
+		                driver.findElement(By.id("validationCustom03")).sendKeys("9878906789");
+		                driver.findElement(By.id("validationCustom04")).sendKeys("abcd");
+		    
+		                driver.findElement(By.id("submitRegButton")).click();
+		                
+		                
+	}
 }
