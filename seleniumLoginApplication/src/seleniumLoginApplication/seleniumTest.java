ca1c425ba1f0b5a2c42b88fd07c0db9687791c3e
Neha031193 <bnehah@gmail.com>
2019-07-25 14:52:56 +0530

    commiting changes in test-case

--- seleniumLoginApplication/src/seleniumLoginApplication/seleniumTest.java
+++ seleniumLoginApplication/src/seleniumLoginApplication/seleniumTest.java
@@ -37,25 +37,20 @@ public class seleniumTest {
 		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		 
 		                WebDriver driver = new ChromeDriver();
+		       		 
+		                driver.get("http://127.0.0.1:5500/");
+		                Thread.sleep(1000);
+		                driver.findElement(By.linkText("Register")).click();
 		 
-//		                driver.get("http://127.0.0.1:5500/");
-		                driver.get("http://dev2.app.cloudlytics.com/main");
 		                Thread.sleep(1000);
-//		                driver.findElement(By.linkText("Register")).click();
-		                driver.findElement(By.id("loginBtn")).click();
-		                Thread.sleep(1000);
-		                driver.findElement(By.id("root")).click();
-		                driver.findElement(By.name("emailId")).sendKeys("kaustubh.barhate@blazeclan.com");
-		                driver.findElement(By.name("password")).sendKeys("Demo@123");
-//		                driver.findElement(By.className("login-btn")).click();
-		                //		                driver.findElement(By.id("validationCustom01")).sendKeys("abcd");
-//		                driver.findElement(By.id("validationCustom02")).sendKeys("efgh");
-//		                driver.findElement(By.id("validationCustomUsername")).sendKeys("abc@gmail.com");
-//		                driver.findElement(By.id("validationCustom03")).sendKeys("9878906789");
-//		                driver.findElement(By.id("validationCustom04")).sendKeys("abcd");
-//		    
-//		                driver.findElement(By.id("submitRegButton")).click();
-		                
+		                driver.findElement(By.id("validationCustom01")).sendKeys("abcd");
+		                driver.findElement(By.id("validationCustom02")).sendKeys("efgh");
+		                driver.findElement(By.id("validationCustomUsername")).sendKeys("abc@gmail.com");
+		                driver.findElement(By.id("validationCustom03")).sendKeys("9878906789");
+		                driver.findElement(By.id("validationCustom04")).sendKeys("abcd");
+		    
+		                driver.findElement(By.id("submitRegButton")).click();
+		                          
 		                
 	}
 }
